86047bf589ed4d27420cd865f48cc9b9e33fd89b
emgardner <gardner.ethan10@gmail.com>
2025-08-19 21:15:42 -0400

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: cloudflare()
+});
